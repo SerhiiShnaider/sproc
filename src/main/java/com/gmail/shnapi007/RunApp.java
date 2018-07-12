21dc216df7763ec51e7aa3de65022a9fb0dfb12e
SerhiS <serhioeskobar@gmail.com>
2018-07-12 21:16:29 +0300

    Added H2 database and profiles: h2, {mysql,default}

--- src/main/java/com/gmail/shnapi007/RunApp.java
+++ src/main/java/com/gmail/shnapi007/RunApp.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -28,18 +29,41 @@ public class RunApp {
   }
 
   @Bean(name = "dataSource")
-  public DataSource getDataSource() {
+  @Profile({"mysql", "default"})
+  public DataSource getMysqlDataSource() {
     DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
     // See: application.properties
-    dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-    dataSource.setUrl(env.getProperty("spring.datasource.url"));
-    dataSource.setUsername(env.getProperty("spring.datasource.username"));
-    dataSource.setPassword(env.getProperty("spring.datasource.password"));
+    dataSource.setDriverClassName(env.getProperty("spring.datasource.mysql.driver-class-name"));
+    dataSource.setUrl(env.getProperty("spring.datasource.mysql.url"));
+    dataSource.setUsername(env.getProperty("spring.datasource.mysql.username"));
+    dataSource.setPassword(env.getProperty("spring.datasource.mysql.password"));
 
     System.out.println("## getDataSource: " + dataSource);
-
     return dataSource;
   }
 
+  /*
+  @Bean
+  @ConditionalOnExpression("${h2.web.enabled:true}")
+  public Server h2WebServer() throws SQLException {
+    return Server.createWebServer("-web", "-webAllowOthers", "-webPort", "1488").start();
+  }*/
+
+
+  // use --spring.profiles.active=h2
+  @Bean(name = "dataSource")
+  @Profile("h2")
+  public DataSource getH2DataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+    // See: application.properties
+    dataSource.setDriverClassName(env.getProperty("spring.datasource.h2.driver-class-name"));
+    dataSource.setUrl(env.getProperty("spring.datasource.h2.url"));
+    dataSource.setUsername(env.getProperty("spring.datasource.h2.username"));
+    dataSource.setPassword(env.getProperty("spring.datasource.h2.password"));
+
+    System.out.println("## getDataSource: " + dataSource);
+    return dataSource;
+  }
 }
